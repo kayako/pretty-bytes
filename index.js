527cfc81caadada9b64933ef01587f2fbbacaf08
Harminder Virk <virk.officials@gmail.com>
2017-04-12 16:25:23 +0530

    Es5 Ready
    Since we use uglify, the module needs to be Es5 ready

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
-const UNITS = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
-module.exports = num => {
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var UNITS = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+module.exports = function (num) {
 	if (!Number.isFinite(num)) {
-		throw new TypeError(`Expected a finite number, got ${typeof num}: ${num}`);
+		throw new TypeError('Expected a finite number, got ' + (typeof num === 'undefined' ? 'undefined' : _typeof(num)) + ': ' + num);
 	}
 
-	const neg = num < 0;
+	var neg = num < 0;
 
 	if (neg) {
 		num = -num;
@@ -16,9 +19,9 @@ module.exports = num => {
 		return (neg ? '-' : '') + num + ' B';
 	}
 
-	const exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), UNITS.length - 1);
-	const numStr = Number((num / Math.pow(1000, exponent)).toPrecision(3));
-	const unit = UNITS[exponent];
+	var exponent = Math.min(Math.floor(Math.log(num) / Math.log(1000)), UNITS.length - 1);
+	var numStr = Number((num / Math.pow(1000, exponent)).toPrecision(3));
+	var unit = UNITS[exponent];
 
 	return (neg ? '-' : '') + numStr + ' ' + unit;
 };
