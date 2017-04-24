488fbcd0467d3c90aef811918ca2511b85deb05c
Harminder Virk <virk.officials@gmail.com>
2017-04-24 14:37:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 'use strict';
 
+function isFinite(value) {
+    // 1. If Type(number) is not Number, return false.
+    if (typeof value !== 'number') {
+        return false;
+    }
+    // 2. If number is NaN, +∞, or −∞, return false.
+    if (value !== value || value === Infinity || value === -Infinity) {
+        return false;
+    }
+    // 3. Otherwise, return true.
+    return true;
+};
+
 var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
 
 var UNITS = ['B', 'kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
 
 module.exports = function (num) {
-	if (!Number.isFinite(num)) {
+	if (!isFinite(num)) {
 		throw new TypeError('Expected a finite number, got ' + (typeof num === 'undefined' ? 'undefined' : _typeof(num)) + ': ' + num);
 	}
 
